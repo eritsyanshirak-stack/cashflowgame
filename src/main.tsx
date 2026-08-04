92cdabc53aaa25c9915963daebf7f5d522dc4c61
eritsyanshirak-stack <eritsyanshirak@gmail.com>
2026-08-04 23:22:50 +0700

    feat: mount balance v1.3 controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './v13.css'
 import App from './App'
+import V13Overlay from './V13Overlay'
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
+createRoot(document.getElementById('root')!).render(<StrictMode><App /><V13Overlay /></StrictMode>)
