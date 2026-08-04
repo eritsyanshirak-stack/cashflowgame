20586fba7f3a04db9d7fc377169731fad0f7ef7f
eritsyanshirak-stack <eritsyanshirak@gmail.com>
2026-08-04 23:48:03 +0700

    feat: mount stock sale profit overlay

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import './index.css'
 import './v13.css'
 import App from './App'
 import V13Overlay from './V13Overlay'
+import StockSaleOverlay from './StockSaleOverlay'
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /><V13Overlay /></StrictMode>)
+createRoot(document.getElementById('root')!).render(<StrictMode><App /><V13Overlay /><StockSaleOverlay /></StrictMode>)
