32363cbd9c12afbf17328dcf531d000127528b21
eritsyanshirak-stack <eritsyanshirak@gmail.com>
2026-08-05 00:38:19 +0700

    refactor(v1.4): remove overlay mounts and load core UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './v13.css'
+import './v14.css'
 import App from './App'
-import V13Overlay from './V13Overlay'
-import StockSaleOverlay from './StockSaleOverlay'
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /><V13Overlay /><StockSaleOverlay /></StrictMode>)
+createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
